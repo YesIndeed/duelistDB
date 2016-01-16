ad79fd11cd502e5beade93cb2b880473f2918d67
Slowbro <heroic.plebeian@gmail.com>
2016-01-16 14:16:00 -0600

    Simple verification works.

--- backend.js
+++ backend.js
@@ -1,12 +1,29 @@
+var dataURL = 'http://yugiohprices.com/api/card_data/';
+
 $(document).ready(function(){
 	$('#search').click(function(){
 		var input = $('#searchInput').val();
-		// console.log(input);
-		// $(document.body).append("<div>input</div>");
+		/*console.log("spaghetti " + input);
+		$(document.body).append("<div>input</div>");
 		var request = new XMLHttpRequest();
-		request.open('GET', 'http://private-anon-5cal96e2f-yugiohprices.apiary-mock.com/api/' + input);
+		request.open('GET', 'http://yugiohprices.com/api/card_data/' + input);*/
+		
+		var xhr = $.ajax(dataURL + input, {dataType : 'json'});
+		xhr.done(function(data, text_status, jqXhr){
+			console.log(data);
+			console.log(text_status);
+			if (text_status == "success")
+			{
+				// Show card information.
+				$(document.body).append("<div>" + input + "</div>");
+			}
+			else
+			{
+				// Card could not be found. Do something.
+			}
+		});
 
-		request.onreadystatechange = function() {
+		/*request.onreadystatechange = function() {
 			if (this.readyState === 4) {
 				console.log('Status:', this.status);
 				console.log('Headers:', this.getAllResponseHeaders());
@@ -15,5 +32,6 @@ $(document).ready(function(){
 		};
 
 		request.send();
+		*/
 	});	
 });
